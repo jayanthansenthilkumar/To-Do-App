124cd3407a2a619e4b45639cc3d8685cfd2dff26
Jayanthan S <144931034+jayanthansenthilkumar@users.noreply.github.com>
2025-04-18 17:40:01 +0530

    Removed unwanted lines

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 require('./db/mongo');
 const todoRoutes = require('./routes/todo');
-
 const app   = express();
 app.use(express.json());
 app.use('/todo', todoRoutes);
-
 app.listen(61830, () => {
     console.log(`Server is running on http://localhost:61830`);
     }
